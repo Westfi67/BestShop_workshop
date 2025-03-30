8a1b3790226c33c45a141d29381c8bb3187b9b98
Westfi67 <westfi@outlook.cz>
2025-03-30 20:24:42 +0200

    Creating environment configuration

---
+++ vite.config.js
@@ -0,0 +1,8 @@
+// vite.config.js
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+      // Replace with the path to your folder containing index.html
+  root: './BestShop_RWD',
+ 
+})
